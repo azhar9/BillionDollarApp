fbb940ad7c778f8f814e4b2577ee0c780513dbfd
azharsyed9 <azhar@taplane.com>
2023-02-17 04:51:09 -0500

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World V1
+          Hello World V2
         </a>
       </header>
     </div>
